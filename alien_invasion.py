f1b78a8f30aea11f219e37e0c6beae33ce841403
Vardhaman Patel <vardhmanpatel733@gmail.com>
2023-10-24 18:37:24 +0530

    Creating a Pygame Window and Responding to User Input

---
+++ alien_invasion.py
@@ -0,0 +1,8 @@
+import sys
+import pygame
+
+class AlienInvasion:
+
+    def __init__(self):
+        pygame.init()  #function initializes the background settings that Pygame needs to work properly
+
